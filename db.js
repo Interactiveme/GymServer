933122ef8e85cd04253b7374ca1c06b674a91f81
Joseph Smith <josephsmith@Josephs-MBP.fritz.box>
2018-02-06 22:20:17 +1300

    added db

--- db.js
+++ db.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/workout_app', { useMongoClient: true });
+//mongoose.connect('mongodb://localhost/workout_app', { useMongoClient: true });
+mongoose.connect('mongodb://admin:FujiFilmX100@ds125058.mlab.com:25058/heroku_2zrz1l4v', { useMongoClient: true });
